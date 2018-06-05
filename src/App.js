4737354978a1876a0e7877ba55ef25312b1c1b63
Peter Marsh <p.marsh@mac.com>
2018-06-05 12:50:44 -0400

    removed comments and formatted

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ class TodoItem extends Component {
   constructor(props) {
     super(props);
   }
- 
 
   render() {
     return (
-      <li className={this.props.completed ? ("completed") : ("")}>
+      <li className={this.props.completed ? "completed" : ""}>
         <div className="view">
-         {this.props.completed ? (<input onChange={this.props.handleCheck} className="toggle" type="checkbox" defaultChecked />) : (<input onChange={this.props.handleCheck} className="toggle" type="checkbox" />)}
+          {this.props.completed ? (
+            <input
+              onChange={this.props.handleCheck}
+              className="toggle"
+              type="checkbox"
+              defaultChecked
+            />
+          ) : (
+            <input
+              onChange={this.props.handleCheck}
+              className="toggle"
+              type="checkbox"
+            />
+          )}
           <label>{this.props.text}</label>
           <button className="destroy" onClick={this.props.handleDelete} />
         </div>
@@ -30,7 +42,15 @@ class TodoList extends Component {
     return (
       <section className="main">
         <ul className="todo-list">
-        {this.props.todos.map( todo => <TodoItem key={todo.id} text={todo.title} completed={todo.completed} handleCheck={this.props.handleCheck(todo.id)} handleDelete={this.props.handleDelete(todo.id)} /> )}
+          {this.props.todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              text={todo.title}
+              completed={todo.completed}
+              handleCheck={this.props.handleCheck(todo.id)}
+              handleDelete={this.props.handleDelete(todo.id)}
+            />
+          ))}
         </ul>
       </section>
     );
@@ -40,43 +60,50 @@ class TodoList extends Component {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {todos: toDoList}
+    this.state = { todos: toDoList };
   }
+
   handleCheck = id => e => {
-    // If the todo we are iterating over has an id that matches the id we baked into the
-    // event handler...
     const { todos } = this.state;
-    this.setState({todos: todos.map(todo => todo.id === id ? {
-      // make a copy of the todo...
-      ...todo,
-      // but change completed to be the opposite of what it was originally;
-      completed: !todo.completed
-      // otherwise, return the original todo, untouched
-    } : todo)})
-  }
+    this.setState({
+      todos: todos.map(
+        todo =>
+          todo.id === id
+            ? {
+                ...todo,
+                completed: !todo.completed
+              }
+            : todo
+      )
+    });
+  };
+
   handleDelete = id => e => {
     const { todos } = this.state;
-    this.setState({todos: todos.filter(todo => todo.id !== id)})
-  }
- handleDeleteCompleted = e => {
-   const { todos } = this.state;
-   this.setState({todos: todos.filter(todo => todo.completed === false)})
- }
-  handlePress = (e) => {
+    this.setState({ todos: todos.filter(todo => todo.id !== id) });
+  };
+
+  handleDeleteCompleted = e => {
+    const { todos } = this.state;
+    this.setState({ todos: todos.filter(todo => todo.completed === false) });
+  };
+
+  handlePress = e => {
     let updatedTodoList = this.state.todos.slice();
     let newTodo = {
-      "userId": 1,
-      "id": this.state.todos.length ? this.state.todos[this.state.todos.length - 1].id + 1 : 1,
-      "title": e.target.value,
-      "completed": false
+      userId: 1,
+      id: this.state.todos.length
+        ? this.state.todos[this.state.todos.length - 1].id + 1
+        : 1,
+      title: e.target.value,
+      completed: false
     };
     updatedTodoList.push(newTodo);
     if (e.key === "Enter" && e.target.value !== "") {
-      this.setState({todos: updatedTodoList});
-      // e.preventDefault();
+      this.setState({ todos: updatedTodoList });
       document.getElementById("text").value = "";
-      }
     }
+  };
   render() {
     return (
       <section className="todoapp">
@@ -90,17 +117,25 @@ class App extends Component {
             autoFocus
           />
         </header>
-        {/* This section should be hidden by default and shown when there are todos */}
-       <TodoList todos={this.state.todos} handleCheck={this.handleCheck} handleDelete={this.handleDelete} />
-        {/* This footer should hidden by default and shown when there are todos */}
+
+        <TodoList
+          todos={this.state.todos}
+          handleCheck={this.handleCheck}
+          handleDelete={this.handleDelete}
+        />
         <footer className="footer">
-          {/* This should be `0 items left` by default */}
           <span className="todo-count">
-            <strong>{this.state.todos.filter(todo => todo.completed === false).length}</strong> item(s) left
+            <strong>
+              {this.state.todos.filter(todo => todo.completed === false).length}
+            </strong>{" "}
+            item(s) left
           </span>
-          {/* Remove this if you don't implement routing */}
-          {/* Hidden if no completed items are left ↓ */}
-          <button onClick={this.handleDeleteCompleted} className="clear-completed">Clear completed</button>
+          <button
+            onClick={this.handleDeleteCompleted}
+            className="clear-completed"
+          >
+            Clear completed
+          </button>
         </footer>
       </section>
     );
